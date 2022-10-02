55d58fb0c3690a5064c6b7634aae601b8b2c768f
Quelleris <74406524+Quelleris@users.noreply.github.com>
2022-10-02 19:22:14 +0200

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+const navbar = document.querySelector("[data-navbar]");
+const navToggle = document.querySelector(".hamburger-btn");
+
+navToggle.addEventListener("click", () => {
+  if (navbar.classList.contains("active")) {
+    navbar.classList.remove("active");
+    navToggle.classList.remove("active");
+  } else {
+    navbar.classList.add("active");
+    navToggle.classList.add("active");
+  }
+});
